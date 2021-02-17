915a3b83479329331d5fdc4334b11c0e919e495b
Zohar Jackson <Jakobovski@users.noreply.github.com>
2021-02-17 13:01:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 sudo apt-get install git-gui
+
+# Setup git autocomplete
+wget https://github.com/git/git/blob/master/contrib/completion/git-completion.tcsh -P ~/; mv ~/git-completion.tcsh ~/.git-completion.tcsh
+wget https://github.com/git/git/blob/master/contrib/completion/git-completion.bash -P ~/; mv ~/git-completion.bash ~/.git-completion.bash
+
