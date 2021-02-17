5e7ebb7a687efe20717b9ea1105b75d3bf41dbb3
Zohar Jackson <Jakobovski@users.noreply.github.com>
2021-02-17 13:27:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-sudo apt-get install git-gui
+sudo apt-get -y install git-gui
+
+# install lazy git
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt-get update
+sudo apt-get -y install lazygit
+
 
 # Setup git autocomplete
 wget https://github.com/git/git/blob/master/contrib/completion/git-completion.tcsh -P ~/; mv ~/git-completion.tcsh ~/.git-completion.tcsh
