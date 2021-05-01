a09d7e24560e2c04dda5f295657876bee402a285
Zohar Jackson <Jakobovski@users.noreply.github.com>
2021-05-01 13:38:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 sudo apt-get -y install lazygit tcsh htop git git-gui
 
+#  Setup tcshrc
+wget -O ~/.tcshrc https://github.com/Jakobovski/linux-config/blob/main/.tcshrc
 
 # Setup git autocomplete
 wget https://github.com/git/git/blob/master/contrib/completion/git-completion.tcsh -P ~/; mv ~/git-completion.tcsh ~/.git-completion.tcsh
