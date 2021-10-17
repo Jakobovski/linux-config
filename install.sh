9a316ed93b5d7fa1ea32c783459f3fb5aea67200
Zohar Jackson <Jakobovski@users.noreply.github.com>
2021-10-17 13:44:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,14 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 sudo gem install colorls
 
 #  Setup shells
 wget -O ~/.tcshrc https://raw.githubusercontent.com/Jakobovski/linux-config/main/.tcshrc
 wget -O ~/.zshrc https://raw.githubusercontent.com/Jakobovski/linux-config/main/.zshrc
 wget -O ~/.zshrc https://raw.githubusercontent.com/Jakobovski/linux-config/main/.p10k.zsh
+~/.fzf/install
 
 
 
