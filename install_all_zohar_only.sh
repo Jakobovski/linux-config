36760d7388fb18fd761c0688a6fd223d79851e44
Zohar Jackson <Jakobovski@users.noreply.github.com>
2021-11-16 17:04:03 +0200

    Create install_all_zohar_only.sh

---
+++ install_all_zohar_only.sh
@@ -0,0 +1,12 @@
+wget -O setuplinux.sh https://raw.githubusercontent.com/Jakobovski/linux-config/main/setuplinux.sh; source setuplinux.sh
+
+# Create SSH key if no SSH dir
+DIR="~/.ssh/"
+if [ -d "$DIR" ]; then
+  ssh-keygen -t ed25519 -C "zoharj@gmail.com"
+  ssh-add ~/.ssh/
+fi
+
+# Setup git
+git config --global user.email "zoharj@gmail.com"
+git config --global user.name "Zohar Jackson"
